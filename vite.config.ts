127ba56c322307042cec81819d08f24208caf0f7
safal098 <sharmasafal098@gmail.com>
2026-05-01 20:30:43 +0545

    Trigger deployment after repo visibility change

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Triggering deployment after visibility change
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/3d-portfolio/",
